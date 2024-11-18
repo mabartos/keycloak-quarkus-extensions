78ba4104754390330b40b79353c70fc887a10b70
Martin Bartoš <mabartos@redhat.com>
2024-11-18 19:47:43 +0100

    CLI - add List implementation
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- cli/src/main/java/org/keycloak/extensions/command/List.java
+++ cli/src/main/java/org/keycloak/extensions/command/List.java
@@ -1,5 +1,6 @@
 package org.keycloak.extensions.command;
 
+import org.keycloak.extensions.MavenInvoker;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "list", mixinStandardHelpOptions = true)
@@ -7,7 +8,7 @@ public class List implements Runnable {
 
     @Override
     public void run() {
-        System.err.println("LIST");
+        MavenInvoker.invoke(MavenInvoker.RUNTIME_POM, "quarkus:list-extensions");
         System.exit(0);
     }
 }
