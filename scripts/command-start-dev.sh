27254f1cfea405de08341f12b0a1c7079ba9dab6
Martin Bartoš <mabartos@redhat.com>
2024-12-19 09:00:04 +0100

    feat: start-dev able to specify version of extended Keycloak
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- scripts/command-start-dev.sh
+++ scripts/command-start-dev.sh
@@ -4,14 +4,55 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 source "$ROOT_DIR/scripts/utils.sh"
 
-handle_command_start_dev() {
-    # Find the Keycloak distribution zip file in the target directory
-    keycloak_zip=$(find "$ROOT_DIR" -maxdepth 1 -name 'keycloak-extended-*.zip' | head -n 1)
+show_help_start_dev() {
+    echo "Execute the generated Keycloak distribution in development mode."
+    echo
+    echo "Usage: ./kc-extensions.sh start-dev [OPTIONS]"
+    echo
+    echo "Options:"
+    echo "  --version <version>             Specify the version of generated extended Keycloak distribution. Defaults to Keycloak version from 'pom.xml' if not provided."
+    echo "  -h, --help                      Displays this help message."
+}
 
-    # Check if the zip file was found
-    if [ -z "$keycloak_zip" ]; then
-        echo "Error: No Keycloak distribution zip file found in target directory."
-        exit 1
+handle_command_start_dev() {
+    version=""
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        --version*)
+            # Extract value from option
+            version="${1#*=}"
+            if [[ -z "$version" || "$version" == "$1" ]]; then
+                echo "Error: Missing value for --version."
+                exit 1
+            fi
+            echo "Extended Keycloak version set to: $version"
+            ;;
+        -h | --help)
+            show_help_start_dev
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Type ./kc-extensions.sh start-dev --help' for available options."
+            exit 1
+            ;;
+        esac
+        shift
+    done
+
+    # If no version is provided, get the one from pom.xml
+    if [[ -z "$version" ]]; then
+        version=$(get_keycloak_version_from_pom)
+        echo "Using extended Keycloak version from pom.xml: $version"
+    fi
+
+    keycloak_zip="$ROOT_DIR"/keycloak-extended-"$version".zip
+
+    # Check whether zip exists
+    if [ ! -f "$keycloak_zip" ]; then
+        echo "Error: No Keycloak distribution zip file found in target directory. ("$keycloak_zip")"
+        exit 1;
     fi
 
     rm -rf "$ROOT_DIR"/target/
