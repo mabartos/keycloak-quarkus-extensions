3b25186562d6f5712f688991ab8dc6d85fd53b5c
Martin Bartoš <mabartos@redhat.com>
2025-01-07 17:43:39 +0100

    feat: Ability to provide multiple properties to the list command
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- scripts/command-list.sh
+++ scripts/command-list.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 source "$ROOT_DIR/scripts/utils.sh"
 
 handle_command_list() {
-    "$ROOT_DIR"/mvnw -f "$ROOT_DIR"/runtime/pom.xml quarkus:list-extensions
+    "$ROOT_DIR"/mvnw -f "$ROOT_DIR"/runtime/pom.xml quarkus:list-extensions "$@"
 }
 
 handle_command_list "$@"
