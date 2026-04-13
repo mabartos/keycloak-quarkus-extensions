83ec1c3dec6f74d443e3ad18680377a16c0d21f0
Martin Bartoš <mabartos@redhat.com>
2026-04-13 15:56:48 +0200

    Support Keycloak 26.6.0
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- scripts/command-build.sh
+++ scripts/command-build.sh
@@ -20,6 +20,9 @@ get_quarkus_version_for_keycloak() {
     local keycloak_version="$1"
 
     case "$keycloak_version" in
+    26.6.*)
+        echo "3.33.1"
+        ;;
     26.5.*)
         echo "3.27.2"
         ;;
